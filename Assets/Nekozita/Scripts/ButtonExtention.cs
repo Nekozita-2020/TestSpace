a9f5532cf9ac9eb94353625cbfa9ce704b45ef55
ネコジタ <>
2022-05-14 17:53:18 +0900

    【実装】 ・TMP Proインポート ・ButtonExtention雛形を作成

---
+++ Assets/Nekozita/Scripts/ButtonExtention.cs
@@ -0,0 +1,13 @@
+namespace Nekozita
+{
+    using UnityEditor;
+    using UnityEngine.UI;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class ButtonExtention : Button
+    {
+
+    }
+}
