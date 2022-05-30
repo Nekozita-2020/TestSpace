eeb1422cb2ea390f1da4276ed41c6149f2b75f90
ネコジタ <>
2022-05-30 23:13:47 +0900

    【実装】 ・Down, Upをオーバーライド

--- Assets/Nekozita/Scripts/ButtonExtention.cs
+++ Assets/Nekozita/Scripts/ButtonExtention.cs
@@ -5,9 +5,56 @@ namespace Nekozita
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.EventSystems;
 
-    public class ButtonExtention : Button
+
+    /// <summary>
+    /// ボタン拡張
+    /// </summary>
+    public class ButtonExtention : Button , IPointerDownHandler, IPointerUpHandler
     {
+        /// <summary>
+        /// Buttonが押下された時
+        /// </summary>
+        /// <param name="eventData"></param>
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
 
+            Debug.Log("OnPointerDown");
+        }
+
+        /// <summary>
+        /// Buttonが押下から解除された時
+        /// </summary>
+        /// <param name="eventData"></param>
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            base.OnPointerUp(eventData);
+
+            Debug.Log("OnPointerUp");
+        }
+
+        /// <summary>
+        /// Buttonがクリックされた時
+        /// </summary>
+        /// <param name="eventData"></param>
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            base.OnPointerClick(eventData);
+
+            Debug.Log("OnPointerClick");
+        }
+
+        /// <summary>
+        /// サブミットボタン（リターンキーやスペースキー）が押されたとき
+        /// </summary>
+        /// <param name="eventData"></param>
+        public override void OnSubmit(BaseEventData eventData)
+        {
+            base.OnSubmit(eventData);
+
+            Debug.Log("OnSubmit");
+        }
     }
 }
